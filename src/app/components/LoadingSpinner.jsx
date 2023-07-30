d4d0cf0f509eb5a69a824a774bcbad83882268c5
Snoopy <hassan.zaib223@gmail.com>
2023-07-30 08:09:16 -0700

    Update LoadingSpinner.jsx

--- src/app/components/LoadingSpinner.jsx
+++ src/app/components/LoadingSpinner.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const LoadingSpinner = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="border-t-4 border-gray-300 border-solid rounded-full animate-spin h-12 w-12"></div>
+    <div className="flex justify-center items-center h-screen w-full">
+      <div className="border-t-4 border-red-800 border-solid rounded-full animate-spin h-20 w-20"></div>
     </div>
   );
 };
